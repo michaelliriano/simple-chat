6a03e91e6697f5be72239e518ea40e73caf220d7
michaelliriano <mikethecoder96@gmail.com>
2022-05-07 00:56:31 -0400

    update fe build

--- ui/src/context/socket.tsx
+++ ui/src/context/socket.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react';
 import io from 'socket.io-client';
 
-const ENDPOINT = process.env.REACT_APP_SERVER_URL || `http://localhost:${2000}`;
+const ENDPOINT = process.env.REACT_APP_SERVER_URL || `http://localhost:${8081}`;
 
 export const socket = io(ENDPOINT, { transports: ['websocket'] });
 export const SocketContext = createContext(socket);
